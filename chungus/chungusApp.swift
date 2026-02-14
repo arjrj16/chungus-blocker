b2f3961ca72b26fb36f23351b4afad489809d717
Arjun Melwani <arjunmelwani@gmail.com>
2026-02-14 04:36:23 -0800

    Initial Commit

---
+++ chungus/chungusApp.swift
@@ -0,0 +1,17 @@
+//
+//  chungusApp.swift
+//  chungus
+//
+//  Created by Arjun Melwani on 2/14/26.
+//
+
+import SwiftUI
+
+@main
+struct chungusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
